b2829b333d8831ccaa162b6eb61f739691f5612f
GiuseppeMandriani <giuseppe.mandriani@gmail.com>
2021-04-22 16:42:15 +0200

    first methods

--- js/script.js
+++ js/script.js
@@ -6,8 +6,40 @@ const app = new Vue({
     el: '#app',
 
     data: {
+        todos:[
+            {
+                text: 'Decidere cosa inserire',
+                completed: false,
+            },
+            {
+                text: 'Capire come inserire la data e l \' ora attuale',
+                completed: false,
+            },
+        ],
 
+        newTodo:'',
     },
+
+    methods: {
+
+        // Function per aggiungere un elemento alla lista
+        addTodo(){
+            // console.log('add new item');
+
+            if(this.newTodo !== ''){
+                this.todos.push({
+                    text: this.newTodo,
+                    completed: false
+                });
+
+            // Reset dell'input dopo averlo inserito
+            this.newTodo = '';
+
+            // Focus
+            this.$refs.todoInput.focus(); // Per far tornare il focus all'unput dopo averlo inserito
+            }
+        },
+    }
 });
     
     
